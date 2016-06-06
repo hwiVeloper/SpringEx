5192919d68e96dbf1189b7589bbcd2d33514b1d7
hwi <zziller03@gmail.com>
2016-06-06 18:39:35 +0900

    20160606

--- src/main/java/com/hwi/ex/MessageBeanKr.java
+++ src/main/java/com/hwi/ex/MessageBeanKr.java
@@ -2,6 +2,6 @@ package com.hwi.ex;
 
 public class MessageBeanKr implements MessageBean {
     public void sayHello(String name) {
-    	System.out.println("�ȳ��ϼ���, " + name + " �� .");
+    	System.out.println("안녕하세요, " + name + " 님.");
     }
 }
