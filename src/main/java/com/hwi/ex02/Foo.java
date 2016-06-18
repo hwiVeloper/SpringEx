b35bf4443617ea3d8968fb5c4f490016bb25ee78
hwi <zziller03@gmail.com>
2016-06-18 18:18:35 +0900

    20160618_1

---
+++ src/main/java/com/hwi/ex02/Foo.java
@@ -0,0 +1,14 @@
+package com.hwi.ex02;
+
+public class Foo {
+	private Bar bar;
+	//생성자를 통한 주입
+	public Foo(Bar bar){
+		this.bar = bar;
+	}
+	
+	//설정 메서드를 통한 주입
+	public setBar(Bar bar){
+		this.bar = bar;
+	}
+}
